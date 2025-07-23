217639077d02f91020893a6152153fb88d1847cb
Joshua Clay <josh.t.clay1995@gmail.com>
2025-07-23 06:42:16 -0700

    Implemented combineUsers function and passed all tests

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+require('datejs'); // Enables Date.today() etc.
+
+function combineUsers(...args) {
+  // Initialize Return Object
+  const combinedObject = { users: [] };
+
+  //Merge Arrays
+  combinedObject.users = [].concat(...args);
+
+  //Add merge_date
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy")
+
+  return combinedObject;
+}
 
 
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+module.exports = { combineUsers };

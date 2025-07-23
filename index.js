8aa1ae6c93af4cd923bb7d401ba9d03c6fab33a0
Joshua Clay <josh.t.clay1995@gmail.com>
2025-07-23 07:01:21 -0700

    Fixed export and removed datejs dependency

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-require('datejs');
-
 function combineUsers(...args) {
+  // Initialize Return Object
   const combinedObject = { users: [] };
+
+  // Merge Arrays
   combinedObject.users = [].concat(...args);
-  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  // Add merge_date using native Date
+  const today = new Date();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  const year = today.getFullYear();
+  combinedObject.merge_date = `${month}/${day}/${year}`;
+
   return combinedObject;
 }
 
-module.exports = combineUsers; 
+module.exports = { combineUsers };

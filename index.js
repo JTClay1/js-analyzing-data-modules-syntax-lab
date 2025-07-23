c3db1af87041d6d77d05c769e8b256bf68d41754
Joshua Clay <josh.t.clay1995@gmail.com>
2025-07-23 06:52:06 -0700

    Final export fix for main branch

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-require('datejs'); // Enables Date.today() etc.
+require('datejs');
 
 function combineUsers(...args) {
-  // Initialize Return Object
   const combinedObject = { users: [] };
-
-  //Merge Arrays
   combinedObject.users = [].concat(...args);
-
-  //Add merge_date
-  combinedObject.merge_date = Date.today().toString("M/d/yyyy")
-
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
   return combinedObject;
 }
 
-
-module.exports = { combineUsers };
+module.exports = combineUsers; 
